8b1c9a99b35c0b6b3f6cb61461297a42f245a572
Nahuel Grijalba <nahug07@gmail.com>
2022-02-14 11:42:59 -0300

    db terminada

--- api/src/db.js
+++ api/src/db.js
@@ -30,10 +30,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Character } = sequelize.models;
+const { Character, Occupation } = sequelize.models; // <-- Me trae los modelos de sequealize
 
-// Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+// Aca vendrian las relaciones                              --> esta es mi tabla intermedia
+// Product.hasMany(Reviews);                              /
+Occupation.belongsToMany(Character, {through: "characters_occupation"});
+Character.belongsToMany(Occupation, {through: "characters_occupation"});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/models/Character.js
+++ api/src/models/Character.js
@@ -4,9 +4,43 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('character', {
+    id: {
+      type: DataTypes.UUID, // Genera un id random con letras y números que va a ser único,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,     // No le permito que este vacío
+      primaryKey: true
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    
+    nickname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    birthday: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    status: {
+      type: DataTypes.ENUM('Alive', 'Deceased', 'Presumed dead', 'Unknown'), //datatypes de enumeración (solo pueden ser esos cuatro) 
+      allowNull: true,
+    },
+
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+
+    createdInDb: {
+      type: DataTypes.BOOLEAN,      //con esta prop acceso fácil a los creados en db
+      allowNull: false,
+      defaultValue: true,
+    },
+
   });
 };
